db6e1196d65c97acbc84d0ec39ac6032f61e5aa5
Zyrenth <71086259+Zyrenth@users.noreply.github.com>
2023-09-20 20:43:11 +0200

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,25 @@
 import { logger } from "@vendetta";
 import Settings from "./Settings";
 
+import { findByStoreName, findByProps } from "@vendetta/metro"
+import { after, instead } from "@vendetta/patcher"
+
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        logger.log("shiggy!");
+
+        const UserStore = findByStoreName("UserStore")
+        
+        let patches = [];
+        
+        patches.push(after("getCurrentUser", UserStore, (_, user) => {
+            user.premiumType = 2;
+            return user
+        }));
     },
     onUnload: () => {
-        logger.log("Goodbye, world.");
+        logger.log("leaving or smth");
+        for (const unpatch of patches) unpatch();
     },
     settings: Settings,
 }
